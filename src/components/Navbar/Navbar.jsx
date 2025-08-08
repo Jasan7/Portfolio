3ef672703200dfc40ca07e965422af5b057793e9
Jasandeep Singh <sjasandeep7@gmail.com>
2025-08-08 14:17:01 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>MyPortfolio</div>
+      <nav>
+        <ul className={styles.navlinks}>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#resume">Resume</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
